08e6d8e968464d02f6e83b4aa52efea2bb681de6
ColinVault <160027012+ColinVault@users.noreply.github.com>
2026-01-22 10:52:02 -0500

    finished 6.1.2

--- Lesson1_SetScores/Main.java
+++ Lesson1_SetScores/Main.java
@@ -4,13 +4,31 @@ package Lesson1_SetScores;
 /**
  * Write a description of class Main here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Colin Hawley)
+ * @version (1/22/26)
  */
 public class Main
 {
     public static void main(String[] args)
     {
+        int[] scores = { 80, 95, 82, 67, 100 };
         
+        printScores(scores);
+        
+        int[] newScores = scores;
+        
+        newScores[2] = 72;
+        newScores[4] = 92;
+        
+        printScores(newScores);
+    }
+    
+    public static void printScores(int[] s)
+    {
+        for (int i = 0; i < s.length; i++)
+        {
+            System.out.println(s[i]);
+        }
+        System.out.println();
     }
 }
