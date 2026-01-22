26b35ab68f70d6b16867288804490abd8b845b7e
ColinVault <160027012+ColinVault@users.noreply.github.com>
2026-01-22 10:59:04 -0500

    finished 6.1.3

--- Lesson1_LastElement/Main.java
+++ Lesson1_LastElement/Main.java
@@ -4,22 +4,27 @@ package Lesson1_LastElement;
 /**
  * Write a description of class Main here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Colin Hawley)
+ * @version (1/22/26)
  */
 public class Main
 {
     public static void main(String[] args)
     {
         // TEST DATA. DO NOT CHANGE
-        double[] unitCircle = {0, 1.57, 3.14, 6.28, 7.85};
+        double[] unitCircle = { 0, 1.57, 3.14, 6.28, 7.85 };
         
         //get and print the last element of the array
+        double lastElement =  getLastElement(unitCircle);
+        
+        System.out.println("The final unit circle value is " + lastElement);
     }
 
     public static double getLastElement(double[] arr)
     {
         // Your code goes here!
-        return -1;
+        int i = arr.length - 1;
+        
+        return arr[i];
     }
 }
