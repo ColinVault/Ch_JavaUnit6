f6ccebecaa0c0d8436f532c9050cdeb321e1a9e3
ColinVault <160027012+ColinVault@users.noreply.github.com>
2026-01-22 10:34:22 -0500

    finished 6.1.1

--- Lesson1_OurFirstArray/Main.java
+++ Lesson1_OurFirstArray/Main.java
@@ -4,8 +4,8 @@ package Lesson1_OurFirstArray;
 /**
  * Write a description of class Main here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Colin Hawley)
+ * @version (1/22/26)
  */
 public class Main
 {
@@ -13,7 +13,27 @@ public class Main
     public static void main(String[] args)
     {
         // Create the 3 arrays here
-        
+        String[] city = { "Las Vegas", "Minsk", "Sao Paulo" };
+        int[] population = { 667501, 2038822, 12330000 };
+        double[] sunshine = { 8.11, 1.36, 6.05 };
         // Print all 3 arrays according to the output in the description
+        printPopulation(city, population);
+        printSunshine(city, sunshine);
+    }
+    
+    public static void printPopulation(String[] c, int[] p)
+    {
+        for (int i = 0; i < c.length; i++)
+        {
+            System.out.println(c[i] + "'s population is " + p[i]);
+        }
+    }
+    
+    public static void printSunshine(String[] c, double[] s)
+    {
+        for (int i = 0; i < c.length; i++)
+        {
+            System.out.println("The least amount of sunshine " + c[i] + " gets is " + s[i] + " hours a day");
+        }
     }
 }
