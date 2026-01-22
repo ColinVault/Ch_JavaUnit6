8fdf59d6c5e4c380b9b7af8bf370f962cce89c6d
ColinVault <160027012+ColinVault@users.noreply.github.com>
2026-01-22 11:11:18 -0500

    finished 6.1.4

--- Lesson1_SnapShot/Main.java
+++ Lesson1_SnapShot/Main.java
@@ -4,13 +4,29 @@ package Lesson1_SnapShot;
 /**
  * Write a description of class Main here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Colin Hawley)
+ * @version (1/22/26)
  */
 public class Main
 {
     public static void main(String[] args)
     {
+        String[] screenshot = { "Welcome", "to", "the snap shot", "app!" };
         
+        printScreen(screenshot);
+        
+        screenshot[0] = "Upgrade";
+        screenshot[3] = "premium app!";
+        
+        printScreen(screenshot);
+    }
+    
+    public static void printScreen(String[] s)
+    {
+        for (int i = 0; i < s.length; i++)
+        {
+            System.out.println(s[i]);
+        }
+        System.out.println();
     }
 }
