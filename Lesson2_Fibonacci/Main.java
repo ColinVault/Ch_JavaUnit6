6624edc8c1b65de978b0cd740f346c9bdb6760e2
ColinVault <160027012+ColinVault@users.noreply.github.com>
2026-01-23 11:10:20 -0500

    Fibonacci

--- Lesson2_Fibonacci/Main.java
+++ Lesson2_Fibonacci/Main.java
@@ -4,8 +4,8 @@ package Lesson2_Fibonacci;
 /**
  * Write a description of class Main here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Colin Hawley)
+ * @version (1/23/26)
  */
 public class Main
 {
@@ -16,23 +16,31 @@ public class Main
         final int MAX_TO_GENERATE = 15;
     
         // create the array to hold the sequence of Fibonacci numbers
-    
+        int[] fibonacci = new int[MAX_TO_GENERATE];
     
         //create the first 2 Fibonacci sequence elements
-
-    
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+        
         //create the Fibonacci sequence and store it in int[] sequence
-
+        for (int i = 1; i < fibonacci.length - 1; i++)
+        {
+            int nextNumber = findNextElement(fibonacci, i);
+            fibonacci[i + 1] = nextNumber;
+        }
     
         //print the Fibonacci sequence numbers
-      
+        for (int i = 0; i < fibonacci.length; i++)
+        {
+            System.out.println(fibonacci[i]);
+        }
         
     }
       
     // This method returns the element that comes after element 'toFind'
     public static int findNextElement (int[] arr, int toFind) 
     {
-            
-        return -1;
+        int nextValue = arr[toFind] + arr[toFind - 1];
+        return nextValue;
     }
 }
