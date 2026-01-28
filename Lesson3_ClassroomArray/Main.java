227c480808ce758eea792bd9119c7fc912ae88d5
ColinVault <160027012+ColinVault@users.noreply.github.com>
2026-01-28 09:46:51 -0500

    finished 6.3.3

--- Lesson3_ClassroomArray/Main.java
+++ Lesson3_ClassroomArray/Main.java
@@ -4,16 +4,24 @@ package Lesson3_ClassroomArray;
 /**
  * Write a description of class Main here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Colin Hawley)
+ * @version (1/28/26)
  */
 public class Main
 {
-        public static void main(String[] args)
+    public static void main(String[] args)
     {
-
-
+        Student stan = new Student("Stan", "Marsh", 4);
+        Student kyle = new Student("Kyle", "Broflovski", 4);
+        Student eric = new Student("Eric", "Cartman", 4);
+        Student kenny = new Student("Kenny", "McCormick", 4);
+        Student butters = new Student("Butters", "Scotch", 4);
         
+        Student[] fourthGrade = { stan, kyle, eric, kenny, butters };
         
+        for (Student classmate : fourthGrade)
+        {
+            System.out.println(classmate);
+        }
     }
 }
--- Lesson3_ClassroomArray/Student.java
+++ Lesson3_ClassroomArray/Student.java
@@ -4,10 +4,23 @@ package Lesson3_ClassroomArray;
 /**
  * Write a description of class Student here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Colin Hawley)
+ * @version (1/28/26)
  */
 public class Student
 {
-
+    private String firstName, lastName;
+    private int grade;
+    
+    public Student(String firstName, String lastName, int grade)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = grade;
+    }
+    
+    public String toString()
+    {
+        return "Grade " + this.grade + ": " + this.firstName + " " + this.lastName;
+    }
 }
